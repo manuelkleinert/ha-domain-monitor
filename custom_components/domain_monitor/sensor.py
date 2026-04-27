75ca023775126b5c758dbe4cc231c122dcadb8e6
Manuel Kleinert <info@manuelkleinert.ch>
2026-04-27 11:45:29 +0200

    Update sensor.py

--- custom_components/domain_monitor/sensor.py
+++ custom_components/domain_monitor/sensor.py
@@ -19,17 +19,23 @@ class DomainSensor(Entity):
         self.service = service
         
         host = service["host"]
-        if service["type"] == "http":
+        if service["type"] == "https":
+            name = f"{host} (https)"
+            uid = f"domain_monitor_{host}_https"
+            icon = "mdi:shield-check"
+        elif service["type"] == "http":
             name = f"{host} (http)"
             uid = f"domain_monitor_{host}_http"
+            icon = "mdi:web"
         else:
             port = service["port"]
             name = f"{host} (tcp:{port})"
             uid = f"domain_monitor_{host}_tcp_{port}"
+            icon = "mdi:lan"
             
         self._attr_name = name
         self._attr_unique_id = uid.replace(".", "_")
-        self._attr_icon = "mdi:web" if service["type"] == "http" else "mdi:lan"
+        self._attr_icon = icon
 
     @property
     def state(self):
